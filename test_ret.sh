453cb5355ed0749db3dc37f759ae7466365c1353
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-09 02:57:51 +0900

    add color

--- test_ret.sh
+++ test_ret.sh
@@ -3,4 +3,4 @@ echo $1 | ./$5 > $2
 gcc $2 -o $3
 ./$3
 res=$?
-if [ $res -ne $4 ]; then { echo FAIL; exit 1; }; else echo PASS; fi
+if [ $res -ne $4 ]; then { echo -e "\033[31mFAIL\033[m"; exit 1; }; else echo -e "\033[32mPASS\033[m"; fi
