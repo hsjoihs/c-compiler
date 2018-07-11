1b9837e1695d57f70f70d84db70f35a134112f97
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-12 07:24:25 +0900

    make use of =

--- print_assembly_check009.c
+++ print_assembly_check009.c
@@ -4,19 +4,34 @@
 
 int main()
 {
+	int offset;
 	/* 
-		a = 123, 51 + a 
+		b = 9, a = 41*3, 55 - (b = 4) + a 
 	*/
-	print_header(4);
+	print_header(8);
 
-	push_int(123);
-	write_to_local(-4);
+	//offset = -4; /* a */
+	offset = -8; /* b */
+	push_int(9); /* 9 */
+	write_to_local(offset);/* = */
+	//write_to_local(offset);/* = */
 
-	push_int(51);
-	push_from_local(-4);
-	op_ints("addl");
+	offset = -4; /* a */
+	push_int(41); /* 41 */
+	push_int(3); /* 3 */
+	mul_ints(); /* * */
+	write_to_local(offset); /* = */
+	op_ints("movl"); /* , */
 
-	op_ints("movl");
-	print_footer(4);
+	push_int(55); /* 55 */
+	offset = -8; /* b */
+	push_int(4); /* 4 */
+	write_to_local(offset);/* = */
+	op_ints("subl"); /* - */
+	push_from_local(-4); /* a */
+	op_ints("addl"); /* + */
+
+	op_ints("movl"); /* , */
+	print_footer(8);
 	return 0;
 }
