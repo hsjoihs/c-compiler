59bc7ac68f2e14707cebbfbe9dbf08d19146d557
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-12 01:27:43 +0900

    use fprintf & abort instead of assert

--- template/vector_template.c
+++ template/vector_template.c
@@ -13,8 +13,10 @@ void extend_vector_T(struct vector_T* ptr)
 
 		ptr->vector = realloc(ptr->vector, ptr->_allocated_length * 2 * sizeof(struct T));
 
-		assert("memory ran out when trying to reallocate an empty vector of type T" 
-			&& ptr->vector); /* fails when the memory runs out */
+		if (!(ptr->vector)) { /* fails when the memory runs out */
+			fprintf(stderr, "memory ran out when trying to reallocate an empty vector of type T");
+			abort();
+		}
 
 		ptr->_allocated_length *= 2;
 	}
@@ -30,7 +32,7 @@ void push_vector_T(struct vector_T* ptr, struct T tok)
 struct T pop_vector_T(struct vector_T* ptr)
 {
 	if (ptr->length == 0) {
-		assert("tried to pop an empty vector of type T" && 0);
+		assert("tried to pop an empty vector of type `struct T`" && 0);
 	}
 	--(ptr->length); 
 	return ptr->vector[ptr->length]; /* safe, since it is not yet released or anything */
--- vector.c
+++ vector.c
@@ -1,6 +1,7 @@
 /* AUTOMATICALLY GENERATED. DO NOT EDIT. */
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 #include "vector.h"
 struct vector_Token init_vector_Token(int initial_length)
 {
@@ -17,8 +18,10 @@ void extend_vector_Token(struct vector_Token* ptr)
 
 		ptr->vector = realloc(ptr->vector, ptr->_allocated_length * 2 * sizeof(struct Token));
 
-		assert("memory ran out when trying to reallocate an empty vector of type Token" 
-			&& ptr->vector); /* fails when the memory runs out */
+		if (!(ptr->vector)) { /* fails when the memory runs out */
+			fprintf(stderr, "memory ran out when trying to reallocate an empty vector of type Token");
+			abort();
+		}
 
 		ptr->_allocated_length *= 2;
 	}
@@ -34,7 +37,7 @@ void push_vector_Token(struct vector_Token* ptr, struct Token tok)
 struct Token pop_vector_Token(struct vector_Token* ptr)
 {
 	if (ptr->length == 0) {
-		assert("tried to pop an empty vector of type Token" && 0);
+		assert("tried to pop an empty vector of type `struct Token`" && 0);
 	}
 	--(ptr->length); 
 	return ptr->vector[ptr->length]; /* safe, since it is not yet released or anything */
