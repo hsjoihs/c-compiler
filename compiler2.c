34711d31f9d56a0ca0836e4e89992fd15061ff22
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-13 17:19:19 +0900

    delete unused codes

--- compiler2.c
+++ compiler2.c
@@ -5,61 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-int get_precedence(enum TokenKind k)
-{
-	switch (k) {
-		case OP_PLUS:
-		case OP_MINUS:
-			return -4;
-
-		case OP_ASTERISK:
-		case OP_SLASH:
-		case OP_PERCENT:
-			return -3;
-
-		case LEFT_PAREN:
-			return 123;
-
-		case OP_COMMA:
-			return -14;
-
-		case OP_LT:
-		case OP_GT:
-		case OP_LT_EQ:
-		case OP_GT_EQ:
-			return -6;
-
-		case OP_LSHIFT:
-		case OP_RSHIFT:
-			return -5;
-
-		case OP_AND:
-			return -8;
-
-		case OP_OR:
-			return -9;
-
-		case OP_EQ_EQ:
-		case OP_NOT_EQ:
-			return -7;
-
-		case OP_NOT:
-		case OP_TILDA:
-			return -1;
-
-		case OP_EQ:
-			return -13;
-
-		case IDENT_OR_RESERVED:
-		case RIGHT_PAREN:
-		case END:
-		case LIT_DEC_INTEGER:
-			assert("NOT AN OPERATOR" && 0);
-			break;
-	}
-	assert("unimplemented!!!!" && 0);
-}
-
 void print_op(enum TokenKind kind)
 {
 	switch (kind) {
@@ -132,66 +77,6 @@ void print_op(enum TokenKind kind)
 	}
 
 	assert("unimplemented!!!!" && 0);
-}
-
-void read_all_and_write_code(const char *str)
-{
-	struct vector_Token op_stack = init_vector_Token(0);
-
-	print_prologue(0);
-
-	struct Token tok;
-	do {
-		tok = get_token(&str);
-
-		if (tok.kind == END) {
-			break;
-		}
-
-		if (tok.kind == LIT_DEC_INTEGER) {
-			push_int(tok.int_value);
-		} else if (tok.kind == LEFT_PAREN) {
-			push_vector_Token(&op_stack, tok);
-		} else if (tok.kind == RIGHT_PAREN) {
-			while (op_stack.length > 0 &&
-			       op_stack.vector[op_stack.length - 1].kind != LEFT_PAREN) {
-				print_op(pop_vector_Token(&op_stack).kind);
-			}
-			if (op_stack.length == 0) {
-				fprintf(stderr, "UNMATCHED BRACKET");
-				abort();
-			} else {
-				pop_vector_Token(&op_stack);
-			}
-		} else if (tok.kind == IDENT_OR_RESERVED) {
-			assert("ident pushing unimplemented" && 0);
-		} else { /* operators */
-			while (
-			    op_stack.length > 0 &&
-			    (get_precedence(op_stack.vector[op_stack.length - 1].kind) >
-			         get_precedence(tok.kind) ||
-			     (get_precedence(op_stack.vector[op_stack.length - 1].kind) ==
-			          get_precedence(tok.kind) &&
-			      get_precedence(tok.kind) != -1 &&
-			      get_precedence(tok.kind) != -2 &&
-			      get_precedence(tok.kind) != -13
-			      /* is left-associative */
-			      )) &&
-			    op_stack.vector[op_stack.length - 1].kind != LEFT_PAREN
-			    /* the operator at the top of the operator stack is not a left
-			       bracket */
-			) {
-				print_op(pop_vector_Token(&op_stack).kind);
-			}
-			push_vector_Token(&op_stack, tok);
-		}
-	} while (1);
-
-	while (op_stack.length > 0) {
-		print_op(pop_vector_Token(&op_stack).kind);
-	}
-
-	print_epilogue(0);
 }
 
 void read_all_tokens_debug(const char *str)
@@ -449,7 +334,6 @@ int main(int argc, char const *argv[])
 		print_prologue(0);
 		parse_expression(&tokvec);
 		parse_final(&tokvec);
-		// read_all_and_write_code(str);
 	}
 	return 0;
 }
