7349f77dbb86f0298104baa1f2390145440373fc
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-13 14:07:36 +0900

    merge all the full_compile00n

--- test_all.sh
+++ test_all.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 cd template/; make; cd ../
 make intmap_check
-make full_compile001
-make full_compile002
-make full_compile003
 make print_assembly_check009
-make full_compile003
-make full_compile004
-make full_compile005
-make full_compile006
-make full_compile007
-make full_compile008
+make full_compile
