134015ff2a6e0113adafbf81a7bba0c9e613941e
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-18 18:56:13 +0900

    vector_int and vector_voidptr is currently not in use

--- vector.h
+++ vector.h
@@ -12,23 +12,3 @@ void extend_vector_charptANDint(struct vector_charptANDint* ptr);
 void push_vector_charptANDint(struct vector_charptANDint* ptr, struct charptANDint tok);
 struct charptANDint pop_vector_charptANDint(struct vector_charptANDint* ptr);
 
-struct vector_int {
-	int length;
-	int _allocated_length;
-	int* vector;
-};
-struct vector_int init_vector_int(int initial_length);
-void extend_vector_int(struct vector_int* ptr);
-void push_vector_int(struct vector_int* ptr, int tok);
-int pop_vector_int(struct vector_int* ptr);
-
-struct vector_voidptr {
-	int length;
-	int _allocated_length;
-	void** vector;
-};
-struct vector_voidptr init_vector_voidptr(int initial_length);
-void extend_vector_voidptr(struct vector_voidptr* ptr);
-void push_vector_voidptr(struct vector_voidptr* ptr, void* tok);
-void* pop_vector_voidptr(struct vector_voidptr* ptr);
-
