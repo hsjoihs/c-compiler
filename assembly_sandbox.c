216d4aa6dd4ca3f4d323ce7088afa3fe0eba611f
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-20 22:24:02 +0900

    tweak some more

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -6,18 +6,18 @@
 int main()
 {
     int x;
-    x = 174;
+    x = 172;
     int *y;
     y = &x;
-    return *y;
+    return (*y) + 2;
  }
 */
 int main()
 {
 	gen_prologue(0, "main");
 
-	/* x = 174; */
-	gen_push_int(174);
+	/* x = 172; */
+	gen_push_int(172);
 	gen_write_to_local(-12);
 	gen_discard();
 
@@ -26,9 +26,11 @@ int main()
 	gen_write_to_local_8byte(-8);
 	gen_discard();
 
-	/* return *y; */
+	/* return (*y) + 2; */
 	gen_push_from_local_8byte(-8);
 	gen_peek_and_dereference();
+	gen_push_int(2);
+	gen_op_ints("addl");
 	gen_return_with_label(123);
 
 	gen_epilogue(123);
