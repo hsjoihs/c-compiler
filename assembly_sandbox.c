87036f2459c0d8c8e4f7b24465fe79d1c618ebdd
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-20 22:32:01 +0900

    experiment an example that's more difficult

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -6,33 +6,50 @@
 int main()
 {
     int x;
-    x = 172;
+    x = 86;
     int *y;
     y = &x;
-    return (*y) + 2;
+    int **z;
+    z = &y;
+    return (*y) + (**z) + 2;
  }
 */
 int main()
 {
-	gen_prologue(0, "main");
+	gen_prologue(24, "main");
 
-	/* x = 172; */
-	gen_push_int(172);
-	gen_write_to_local(-12);
+	/* x = 86; */
+	gen_push_int(86);
+	gen_write_to_local(-16);
 	gen_discard();
 
 	/* y = &x; */
-	gen_push_address_of_local(-12);
+	gen_push_address_of_local(-16);
 	gen_write_to_local_8byte(-8);
 	gen_discard();
 
-	/* return (*y) + 2; */
-	gen_push_from_local_8byte(-8);
-	gen_peek_and_dereference();
-	gen_push_int(2);
-	gen_op_ints("addl");
-	gen_return_with_label(123);
+	/* z = &y; */
+	gen_push_address_of_local(-8);
+	gen_write_to_local_8byte(-24);
+	gen_discard();
 
+	/* return (*y) + (**z) + 2; */
+	{
+		/* *y */
+		gen_push_from_local_8byte(-8);
+		gen_peek_and_dereference();
+
+		/* **z */
+		gen_push_from_local_8byte(-24);
+		gen_peek_and_dereference_8byte();
+		gen_peek_and_dereference();
+
+		gen_op_ints("addl");
+		gen_push_int(2);
+		gen_op_ints("addl");
+
+		gen_return_with_label(123);
+	}
 	gen_epilogue(123);
 	return 0;
 }
