8cb02cf34bbcb7d5a61ec0c5040dc3ab0cabe85d
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-21 06:26:56 +0900

    implement is_equal

--- type.c
+++ type.c
@@ -14,3 +14,16 @@ int size_of(struct Type type)
 			return 8;
 	}
 }
+
+int is_equal(struct Type t1, struct Type t2)
+{
+	if(t1.type == INT_ && t2.type == INT_) {
+		return 1;
+	}
+
+	if(t1.type == PTR_ && t2.type == PTR_) {
+		return is_equal(*t1.pointer_of, *t2.pointer_of);
+	}
+
+	return 0;
+}
