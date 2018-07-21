78a6db8e694b4d187933482942dd6cd8cba78f3f
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-21 10:07:47 +0900

    add debug_print_type and pop_type_and_expect

--- type.c
+++ type.c
@@ -36,6 +36,19 @@ int is_equal(struct Type t1, struct Type t2)
 	return 0;
 }
 
+void debug_print_type(struct Type type)
+{
+	switch (type.type) {
+		case PTR_:
+			debug_print_type(*type.pointer_of);
+			fprintf(stderr, "*");
+			return;
+		case INT_:
+			fprintf(stderr, "int");
+			return;
+	}
+}
+
 void push_type(struct TypeCheckerState *ptr_tcs, struct Type type)
 {
 	struct vector type_stack = ptr_tcs->type_stack;
@@ -45,11 +58,25 @@ void push_type(struct TypeCheckerState *ptr_tcs, struct Type type)
 	ptr_tcs->type_stack = type_stack;
 }
 
-struct Type *pop_type(struct TypeCheckerState *ptr_tcs, struct Type type)
+struct Type *pop_type(struct TypeCheckerState *ptr_tcs)
 {
 	struct vector type_stack = ptr_tcs->type_stack;
 	struct Type *p_top_type = pop_vector(&type_stack);
 	return p_top_type;
+}
+
+void pop_type_and_expect(struct TypeCheckerState *ptr_tcs,
+                         struct Type expected_type)
+{
+	struct Type *p_popped_type = pop_type(ptr_tcs);
+	if (!is_equal(*p_popped_type, expected_type)) {
+		fprintf(stderr, "type mismatch: expected `");
+		debug_print_type(expected_type);
+		fprintf(stderr, "` but got `");
+		debug_print_type(*p_popped_type);
+		fprintf(stderr, "`.\n");
+		exit(EXIT_FAILURE);
+	}
 }
 
 void typecheck_push_int(struct TypeCheckerState *ptr_tcs, int num)
