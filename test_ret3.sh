828d627fae17049f07d367828be8df454c7d1ce5
hsjoihs <hs.ioling.hs@gmail.com>
2018-07-29 22:04:34 +0900

    better bash

--- test_ret3.sh
+++ test_ret3.sh
@@ -3,4 +3,4 @@ echo -e $1 | ./$5 > $2
 gcc $2 s/supplement.s -o $3
 ./$3
 res=$?
-if [ $res -ne $4 ]; then { echo -e "\033[31mFAIL\033[m"; exit 1; }; else echo -e "\033[32mPASS\033[m"; fi
+if [ $res -ne $4 ]; then { echo $res; echo $4; echo -e "\033[31mFAIL\033[m"; exit 1; }; else echo -e "\033[32mPASS\033[m"; fi
