b50cf3cd0ed843a17212a6b8cefb725b7ee74338
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-03 02:22:32 +0900

    split the function to call

--- parse_binary_expression.c
+++ parse_binary_expression.c
@@ -99,6 +99,26 @@ struct Expression binary_op_(struct Expression expr, struct Expression expr2,
 	return new_expr;
 }
 
+struct Expression simple_binary_op(struct Expression expr,
+                                   struct Expression expr2, enum TokenKind kind,
+                                   struct ExprInfo exprinfo)
+{
+	struct Expression *ptr_expr1 = calloc(1, sizeof(struct Expression));
+	struct Expression *ptr_expr2 = calloc(1, sizeof(struct Expression));
+	*ptr_expr1 = expr;
+	*ptr_expr2 = expr2;
+
+	struct Expression new_expr;
+	new_expr.details = exprinfo;
+	new_expr.category = SIMPLE_BINARY_EXPR;
+	new_expr.binary_operator = kind;
+	new_expr.ptr1 = ptr_expr1;
+	new_expr.ptr2 = ptr_expr2;
+	new_expr.ptr3 = 0;
+
+	return new_expr;
+}
+
 struct Expression pointer_plusorminus_int(struct Expression expr,
                                           struct Expression expr2,
                                           enum TokenKind kind)
@@ -123,8 +143,7 @@ struct Expression parse_inclusive_OR_expression(struct ParserState *ptr_ps,
 		    remove_leftiness_(parse_exclusive_OR_expression(ptr_ps, &tokvec));
 		expect_type(expr.details, INT_TYPE, 3);
 		expect_type(expr2.details, INT_TYPE, 4);
-		expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-		                  UNASSIGNABLE(INT_TYPE));
+		expr = simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 	}
 	*ptr_tokvec = tokvec;
 	return expr;
@@ -148,8 +167,7 @@ struct Expression parse_exclusive_OR_expression(struct ParserState *ptr_ps,
 		    remove_leftiness_(parse_AND_expression(ptr_ps, &tokvec));
 		expect_type(expr.details, INT_TYPE, 5);
 		expect_type(expr2.details, INT_TYPE, 6);
-		expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-		                  UNASSIGNABLE(INT_TYPE));
+		expr = simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 	}
 	*ptr_tokvec = tokvec;
 
@@ -173,8 +191,7 @@ struct Expression parse_AND_expression(struct ParserState *ptr_ps,
 		    remove_leftiness_(parse_equality_expression(ptr_ps, &tokvec));
 		expect_type(expr.details, INT_TYPE, 7);
 		expect_type(expr2.details, INT_TYPE, 8);
-		expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-		                  UNASSIGNABLE(INT_TYPE));
+		expr = simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 	}
 	*ptr_tokvec = tokvec;
 
@@ -198,8 +215,7 @@ struct Expression parse_equality_expression(struct ParserState *ptr_ps,
 		    remove_leftiness_(parse_relational_expression(ptr_ps, &tokvec));
 		expect_type(expr.details, INT_TYPE, 9);
 		expect_type(expr2.details, INT_TYPE, 10);
-		expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-		                  UNASSIGNABLE(INT_TYPE));
+		expr = simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 	}
 	*ptr_tokvec = tokvec;
 	return expr;
@@ -223,8 +239,7 @@ struct Expression parse_relational_expression(struct ParserState *ptr_ps,
 		    remove_leftiness_(parse_shift_expression(ptr_ps, &tokvec));
 		expect_type(expr.details, INT_TYPE, 11);
 		expect_type(expr2.details, INT_TYPE, 12);
-		expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-		                  UNASSIGNABLE(INT_TYPE));
+		expr = simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 	}
 	*ptr_tokvec = tokvec;
 	return expr;
@@ -247,8 +262,7 @@ struct Expression parse_shift_expression(struct ParserState *ptr_ps,
 		    remove_leftiness_(parse_additive_expression(ptr_ps, &tokvec));
 		expect_type(expr.details, INT_TYPE, 13);
 		expect_type(expr2.details, INT_TYPE, 14);
-		expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-		                  UNASSIGNABLE(INT_TYPE));
+		expr = simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 	}
 	*ptr_tokvec = tokvec;
 	return expr;
@@ -275,8 +289,8 @@ struct Expression parse_additive_expression(struct ParserState *ptr_ps,
 
 		if (is_equal(type1, INT_TYPE)) {
 			if (is_equal(type2, INT_TYPE)) {
-				expr = binary_op_(expr, expr2, kind, SIMPLE_BINARY_EXPR,
-				                  UNASSIGNABLE(INT_TYPE));
+				expr =
+				    simple_binary_op(expr, expr2, kind, UNASSIGNABLE(INT_TYPE));
 			} else if (is_pointer(type2)) {
 				if (kind == OP_MINUS) {
 					fprintf(stderr,
