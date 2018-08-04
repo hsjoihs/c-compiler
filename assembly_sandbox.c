eab3fba8a5554ffb8eecbe6198d4e85856c0c166
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-05 03:13:40 +0900

    experiment

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -8,15 +8,27 @@ int main(){int a[1][2];int (*p)[2];p = a;int *q;q = *(p+1); *q=174; return
 
 int main()
 {
-	gen_prologue(0, "main");
-	puts("  leaq -24(%rbp), %rax\n"
-	     "  movq %rax, -8(%rbp)\n"
-	     "  movq -8(%rbp), %rax\n"
-	     "  addq $8, %rax\n"
-	     "  movq %rax, -16(%rbp)\n"
-	     "  movq -16(%rbp), %rax\n"
-	     "  movl $174, (%rax)\n"
-	     "  movl -16(%rbp), %eax\n"
-	     "  popq %rbp\n"
-	     "  ret\n");
+	gen_prologue(32, "main");
+
+	/* p = a; */
+	gen_push_address_of_local(-24); /* &a[0] */
+	gen_write_to_local_8byte(-8);   /* p= */
+	gen_discard();                  /* ; */
+
+	/* q = *(p+1); */
+	gen_push_from_local_8byte(-8);
+	gen_push_int(8);
+	gen_op_8byte("addq"); /* p+1 :: pointer to array(length 2) */
+
+	/* pointer to array does not change its value when dereferenced */
+	gen_write_to_local_8byte(-16);
+	gen_discard();
+
+	gen_push_from_local_8byte(-16); /* q */
+	gen_push_int(174);              /* 174 */
+	gen_assign_4byte();             /* *q = 174; */
+	gen_discard();
+
+	gen_push_from_local(-16);
+	gen_epilogue(12345);
 }
