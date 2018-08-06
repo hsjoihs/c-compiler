2c05bee5480d0dfdb13e07d88a7e30f84c543985
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-06 13:27:28 +0900

    experiment

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -2,33 +2,88 @@
 #include <stdio.h>
 
 /*
-int main(){int a[1][2];int (*p)[2];p = a;int *q;q = *(p+1); *q=174; return
-**(a+1);}
+char x[3];
+char y[3];
+char foo() { int c = 1; int d = 0; x[d] = y[c]; char e = 3; return e;}
+int main(){ char f = foo(); return 174;}
 */
 
 int main()
 {
-	gen_prologue(32, "main");
+	gen_prologue(32, "foo");
 
-	/* p = a; */
-	gen_push_address_of_local(-24); /* &a[0] */
-	gen_write_to_local_8byte(-8);   /* p= */
-	gen_discard();                  /* ; */
-
-	/* q = *(p+1); */
-	gen_push_from_local_8byte(-8);
-	gen_push_int(8);
-	gen_op_8byte("addq"); /* p+1 :: pointer to array(length 2) */
-
-	/* pointer to array does not change its value when dereferenced */
-	gen_write_to_local_8byte(-16);
+	gen_push_int(1);
+	gen_write_to_local(-4); /* c */
 	gen_discard();
 
-	gen_push_from_local_8byte(-16); /* q */
-	gen_push_int(174);              /* 174 */
-	gen_assign_4byte();             /* *q = 174; */
+	gen_push_int(0);
+	gen_write_to_local(-8); /* d */
 	gen_discard();
 
-	gen_push_from_local(-16);
-	gen_epilogue(12345);
+	gen_push_from_local(-4); /* c */
+	gen_cltq();
+	gen_push_address_of_global("y");
+	gen_op_8byte("addq");
+	gen_pop_to_reg_8byte("rdx");
+	puts("  movb (%rdx), %sil\n");
+
+	gen_push_address_of_global("x");
+	gen_push_from_local(-8);
+	gen_cltq();
+	gen_op_8byte("addq");
+	gen_pop_to_reg_8byte("rdx");
+	puts("	movb	%sil, (%rdx)\n");
+
+	puts("	movb	$3, -9(%rbp)\n"
+	     "  subq $8, %rsp\n"
+	     "	movsbl	-9(%rbp), %eax\n"
+	     "	movl	%eax, (%rsp)\n");
+	gen_epilogue(134);
+
+	/*
+
+
+
+	*/
+	gen_prologue(16, "main");
+	gen_push_int(0);
+	gen_write_to_local(-4);
+	gen_discard();
+
+	puts("	call	" PREFIX "foo\n"
+	     "	movb	%al, -5(%rbp)\n");
+	gen_push_int(174);
+	gen_epilogue(123);
+	gen_global_declaration("x", 3);
+	gen_global_declaration("y", 3);
+	return 0;
+	/*
+	gen_global_declaration(PREFIX "x", 3);
+	gen_global_declaration(PREFIX "y", 3);
+	puts("" PREFIX "foo:\n"
+	     "  pushq %rbp\n"
+	     "  movq %rsp, %rbp\n"
+	     "  movl $1, -4(%rbp)\n"
+	     "  movl $0, -8(%rbp)\n"
+	     "  movl -4(%rbp), %eax\n"
+	     "  cltq\n"
+	     "  movzbl " PREFIX "y(%rax), %edx\n"
+	     "  movl -8(%rbp), %eax\n"
+	     "  cltq\n"
+	     "  movb %dl, " PREFIX "x(%rax)\n"
+	     "  movb $3, -9(%rbp)\n"
+	     "  movzbl -9(%rbp), %eax\n"
+	     "  popq %rbp\n"
+	     "  ret\n");
+	puts(".global " PREFIX "main\n"
+	     "" PREFIX "main:\n"
+	     "  pushq %rbp\n"
+	     "  movq %rsp, %rbp\n"
+	     "  subq $16, %rsp\n"
+	     "  movl $0, %eax\n"
+	     "  call " PREFIX "foo\n"
+	     "  movb %al, -1(%rbp)\n"
+	     "  movl $174, %eax\n"
+	     "  leave\n"
+	     "  ret\n");*/
 }
