1f80d543e59cf43e93d44a92426ec6840d19e45e
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-07 05:24:01 +0900

    experiment

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -2,90 +2,59 @@
 #include <stdio.h>
 
 /*
-char x[3];
-char y[3];
-char foo() { int c = 1; int d = 0; x[d] = y[c]; char e = 3; return e;}
-int main(){ char f; return (f = foo())+171;}
+int foo(char a, char b){
+    int d = 3;
+    char c = a+d;
+    return c*b;
+}
+
+
 */
 
 int main()
 {
-	gen_prologue(32, "foo");
+	puts(
 
-	gen_push_int(1);
-	gen_write_to_local(-4); /* c */
-	gen_discard();
-
-	gen_push_int(0);
-	gen_write_to_local(-8); /* d */
-	gen_discard();
-
-	gen_push_address_of_global("x");
-	gen_push_from_local_4byte(-8); /* d */
-	gen_cltq();
-	gen_op_8byte("addq"); /* &x[d] */
-
-	gen_push_from_local_4byte(-4); /* c */
-	gen_cltq();
-	gen_push_address_of_global("y");
-	gen_op_8byte("addq");
-	gen_peek_and_dereference_1byte(); /* y[c] */
-	gen_assign_1byte();
-	gen_discard();
-
-	gen_push_int(3);
-	gen_write_to_local_1byte(-9);
-	gen_discard();
-
-	gen_push_from_local_1byte(-9);
-	gen_epilogue(134);
-
+	    "" PREFIX "foo:\n"
+	    "  pushq %rbp\n"
+	    "  movq %rsp, %rbp\n"
+	    "  movl %edi, %eax\n"
+	    "  movl %esi, %edx\n"
+	    "  movb %al, -20(%rbp)\n"
+	    "  movl %edx, %eax\n"
+	    "  movb %al, -24(%rbp)\n"
+	    "  movl $3, -4(%rbp)\n"
+	    "  movl -4(%rbp), %eax\n"
+	    "  movl %eax, %edx\n"
+	    "  movzbl -20(%rbp), %eax\n"
+	    "  addl %edx, %eax\n"
+	    "  movb %al, -5(%rbp)\n"
+	    "  movsbl -5(%rbp), %edx\n"
+	    "  movsbl -24(%rbp), %eax\n"
+	    "  imull %edx, %eax\n"
+	    "  popq %rbp\n"
+	    "  ret\n");
 	/*
-
-
-
+	int main()
+	{
+	    char f=3;
+	    return foo(3,4)+150;
+	}
 	*/
 	gen_prologue(16, "main");
-	gen_push_int(0);
-	gen_write_to_local(-4);
+
+	gen_push_int(3);
+	gen_write_to_local_1byte(-1);
 	gen_discard();
 
-	gen_push_ret_of_1byte("foo");
-	gen_write_to_local_1byte(-5);
-	gen_push_int(171);
-	gen_op_ints("addl");
+	gen_push_int(4);
+	gen_pop_to_reg_4byte("esi");
 
-	gen_epilogue(123);
-	gen_global_declaration("x", 3);
-	gen_global_declaration("y", 3);
-	return 0;
-	/*
-	gen_global_declaration(PREFIX "x", 3);
-	gen_global_declaration(PREFIX "y", 3);
-	puts("" PREFIX "foo:\n"
-	     "  pushq %rbp\n"
-	     "  movq %rsp, %rbp\n"
-	     "  movl $1, -4(%rbp)\n"
-	     "  movl $0, -8(%rbp)\n"
-	     "  movl -4(%rbp), %eax\n"
-	     "  cltq\n"
-	     "  movzbl " PREFIX "y(%rax), %edx\n"
-	     "  movl -8(%rbp), %eax\n"
-	     "  cltq\n"
-	     "  movb %dl, " PREFIX "x(%rax)\n"
-	     "  movb $3, -9(%rbp)\n"
-	     "  movzbl -9(%rbp), %eax\n"
-	     "  popq %rbp\n"
-	     "  ret\n");
-	puts(".global " PREFIX "main\n"
-	     "" PREFIX "main:\n"
-	     "  pushq %rbp\n"
-	     "  movq %rsp, %rbp\n"
-	     "  subq $16, %rsp\n"
-	     "  movl $0, %eax\n"
-	     "  call " PREFIX "foo\n"
-	     "  movb %al, -1(%rbp)\n"
-	     "  movl $174, %eax\n"
-	     "  leave\n"
-	     "  ret\n");*/
+	gen_push_int(3);
+	gen_pop_to_reg_4byte("edi");
+
+	gen_push_ret_of_4byte("foo");
+	gen_push_int(150);
+	gen_op_ints("addl");
+	gen_epilogue(1234);
 }
