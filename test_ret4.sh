d41f3a72df10197582ab877f5f60e3e8303eaa48
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-07 07:17:25 +0900

    better shellscript

--- test_ret4.sh
+++ test_ret4.sh
@@ -3,4 +3,4 @@ echo -e $2 | ./out/compiler.out > s/full_compile$1.s
 gcc s/full_compile$1.s s/supplement.s -o out/task$1.out
 ./out/task$1.out
 res=$?
-if [ $res -ne $3 ]; then { echo -e "\033[31mFAIL\033[m"; exit 1; }; else echo -e "\033[32mPASS\033[m"; fi
+if [ $res -ne $3 ]; then { echo $res; echo $3; echo -e "\033[31mFAIL\033[m"; exit 1; }; else echo -e "\033[32mPASS\033[m"; fi
