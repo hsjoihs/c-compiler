176857713bd6d99477cec8c9c95cdf3c6c5322d5
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-07 15:09:07 +0900

    experiment. tested only in OSX

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -1,55 +1,80 @@
 #include "print_x86_64.h"
 #include <stdio.h>
 
+#ifdef OSX
+void gen_str(int strnum, const char *str)
+{
+	printf("//gen_str(%d, %s)\n", strnum, str);
+	printf("L_str%d:\n"
+	       ".asciz \"%s\"\n",
+	       strnum, str);
+}
+
+void gen_push_address_of_str(int strnum)
+{
+	printf("//gen_push_address_of_str(%d)\n", strnum);
+	printf("  subq $8, %%rsp\n"
+	       "  leaq L_str%d(%%rip), %%rax\n"
+	       "  movq %%rax, (%%rsp)\n",
+	       strnum);
+}
+#endif
+
 int main()
 {
-
 	/*
-	int foo(char a, char b){
-	    int d = 3;
-	    char c = a+d;
-	    return c*b;
-	}
+	char foo() { char *x = "1ab"; return x[0]; }
 	*/
 	gen_prologue(0, "foo");
-	gen_write_register_to_local_1byte("edi", -20);
-	gen_write_register_to_local_1byte("esi", -24);
-
-	gen_push_int(3);
-	gen_write_to_local(-4);
+	gen_push_address_of_str(0);
+	gen_write_to_local_8byte(-8); /* x */
 	gen_discard();
 
-	gen_push_from_local_4byte(-4);
-	gen_push_from_local_1byte(-20);
-	gen_op_ints("addl");
-	gen_write_to_local_1byte(-5);
-	gen_discard();
-
-	gen_push_from_local_1byte(-5);
-	gen_push_from_local_1byte(-24);
-	gen_mul_ints();
-	gen_epilogue(1222);
+	gen_push_from_local_8byte(-8);
+	gen_peek_and_dereference_1byte();
+	gen_epilogue(1234);
 	/*
 	int main()
 	{
-	    char f=3;
-	    return foo(f,4)+150;
+	 char *y = "a2b";
+	 int z = 12; char a = y[1];
+	 return (a-foo())*z+162;
 	}
 	*/
-	gen_prologue(16, "main");
-
-	gen_push_int(3);
-	gen_write_to_local_1byte(-1);
+	gen_prologue(32, "main");
+	gen_push_address_of_str(1);
+	gen_write_to_local_8byte(-16); /* y */
 	gen_discard();
 
-	gen_push_int(4);
-	gen_pop_to_reg_4byte("esi");
+	gen_push_int(0);
+	gen_write_to_local(-4);
+	gen_discard();
 
-	gen_push_from_local_1byte(-1);
-	gen_pop_to_reg_4byte("edi");
+	gen_push_int(12);
+	gen_write_to_local(-20); /* z */
+	gen_discard();
 
-	gen_push_ret_of_4byte("foo");
-	gen_push_int(150);
+	gen_push_from_local_8byte(-16); /* y */
+	gen_push_int(1);
+	gen_cltq();
+	gen_op_8byte("addq");             /* y+1 */
+	gen_peek_and_dereference_1byte(); /* y[1] */
+	gen_write_to_local_1byte(-21);    /* a */
+	gen_discard();
+
+	gen_push_from_local_1byte(-21); /* a */
+	gen_write_to_local(-28);        /* (int)a */
+	gen_discard();
+
+	gen_push_from_local_4byte(-28); /* (int)a */
+	gen_push_ret_of_1byte("foo");   /* foo() */
+	gen_op_ints("subl");
+	gen_push_from_local_4byte(-20); /* z */
+	gen_mul_ints();
+	gen_push_int(162);
 	gen_op_ints("addl");
-	gen_epilogue(1234);
+	gen_epilogue(234);
+	gen_str(0, "1ab");
+	gen_str(1, "a2b");
+	return 0;
 }
