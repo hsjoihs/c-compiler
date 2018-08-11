ed1f280f82f2dbc2747cfbed92dd9008a005aafb
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-12 07:57:09 +0900

    Delete unnecessary declaration

--- global_definition.h
+++ global_definition.h
@@ -1,6 +1,5 @@
 #include "parser.h"
 #include "printerstate.h"
 
-void print_string_pool(struct Vector pool);
 void generate(const struct Vector vec);
 struct Vector parse(const struct Token *tokvec);
