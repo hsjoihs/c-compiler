068dc0ff163ed7a5c247f16066b2a88b060ec15b
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-17 07:59:45 +0900

    experiment

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -4,33 +4,68 @@
 int main()
 {
 	/*
-	#include <stdio.h>
-	int main()
-	{
-	    printf("%d %s", 1, "a");
-	    return 174;
-	}
+struct A {
+	char a; // offset: 0
+	int b; // offset: 4
+};
+
+int foo(struct A* p)
+{
+	return p->b;
+}
+
+int main()
+{
+	struct A a;
+	a.b = 3;
+	int b = a.b;
+	int c = foo(&a);
+	return 168+b+c;
+}
+
+
 	*/
-	gen_str(0, "a");
-	gen_str(1, "%d %s");
+
+	gen_prologue(0, "foo");
+	gen_write_register_to_local_8byte("rdi", -8);
+	gen_push_from_local_8byte(-8);
+	gen_push_int(4);
+	gen_cltq();
+	gen_op_8byte("addq");
+	gen_peek_and_dereference_4byte();
+	gen_epilogue(1234);
 
 	gen_prologue(16, "main");
-	gen_push_address_of_str(1);
-	gen_pop_to_reg_8byte("rdi");
 
-	gen_push_int(1);
-	gen_pop_to_reg_4byte("esi");
-	gen_push_address_of_str(0);
-	gen_pop_to_reg_8byte("rdx");
+	gen_push_address_of_local(-16);
+	gen_push_int(4);
+	gen_cltq();
+	gen_op_8byte("addq");
+	gen_push_int(3);
+	gen_assign_4byte();
+	gen_discard();
 
-	gen_push_int(0);
+	gen_push_address_of_local(-16);
+	gen_push_int(4);
+	gen_cltq();
+	gen_op_8byte("addq");
+	gen_peek_and_dereference_4byte();
 	gen_write_to_local(-4);
 	gen_discard();
 
-	gen_push_ret_of_4byte("printf");
+	gen_push_address_of_local(-16);
+	gen_pop_to_reg_8byte("rdi");
+	gen_push_ret_of_4byte("foo");
+	gen_write_to_local(-8);
 	gen_discard();
 
-	gen_push_int(174);
-	gen_epilogue(12345);
+	puts(
+	     "  movl -4(%rbp), %eax\n"
+	     "  addl $168, %eax\n"
+	     "  movl %eax, %edx\n"
+	     "  movl -8(%rbp), %eax\n"
+	     "  addl %edx, %eax\n"
+	     "  leave\n"
+	     "  ret\n");
 	return 0;
 }
