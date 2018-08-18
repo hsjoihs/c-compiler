06ac68cca3663ed2def94b40eab61ca6a7dc2c84
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-18 11:00:22 +0900

    experiment

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -22,28 +22,39 @@ int main()
 
 	*/
 	gen_prologue(0, "foo");
-
-	puts(
-
-	    "  movl $3, %eax\n"
-	    "  popq %rbp\n"
-	    "  ret\n");
+	gen_push_int(3);
+	gen_epilogue(123);
 
 	gen_prologue(16, "main");
 
-	puts("  movl $24, -4(%rbp)\n"
-	     "  movl $2, -8(%rbp)\n"
-	     "  movl $0, %eax\n"
-	     "  call " PREFIX "foo\n"
-	     "  cmpl $13, %eax\n"
-	     "  jne .L4\n"
-	     "  movl $14, -4(%rbp)\n"
-	     ".L4:\n"
-	     "  movl $150, -8(%rbp)\n"
-	     "  movl -4(%rbp), %edx\n"
-	     "  movl -8(%rbp), %eax\n"
-	     "  addl %edx, %eax\n"
-	     "  leave\n"
-	     "  ret\n");
+	gen_push_int(24);
+	gen_write_to_local(-4); /* a */
+	gen_discard();
+
+	gen_push_int(2);
+	gen_write_to_local(-8); /* b */
+	gen_discard();
+
+	int label = 4;
+	gen_push_ret_of_4byte("foo");
+	printf("  addq $8, %%rsp\n"
+	       "  cmpl $13, -8(%%rsp)\n"
+	       "  jne .L%d\n",
+	       label);
+
+	gen_push_int(14);
+	gen_write_to_local(-4); /* a */
+	gen_discard();
+
+	printf(".L%d:\n", label);
+
+	gen_push_int(150);
+	gen_write_to_local(-8); /* b */
+	gen_discard();
+
+	gen_push_from_local_4byte(-4);
+	gen_push_from_local_4byte(-8);
+	gen_op_ints("addl");
+	gen_epilogue(234);
 	return 0;
 }
