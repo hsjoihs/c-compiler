1a525b9f99355806cfdde4f71c5e1d727cd6c92b
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-22 21:15:04 +0900

    add samples to verifier/typeparse_checker.c

--- verifier/typeparse_checker.c
+++ verifier/typeparse_checker.c
@@ -1,7 +1,7 @@
 #include "../header.h"
 #include <stdio.h>
 
-void check(const char *str)
+void check_declaration(const char *str)
 {
 	fprintf(stderr, "Parsing: %s\n", str);
 	const struct Token *tokvec = read_all_tokens(str);
@@ -17,23 +17,49 @@ void check(const char *str)
 	return;
 }
 
+void check_typename(const char *str)
+{
+	fprintf(stderr, "Parsing: %s\n", str);
+	const struct Token *tokvec = read_all_tokens(str);
+
+	++tokvec; /* skip the dummy token BEGINNING */
+
+	struct Type type = parse_type_name(&tokvec);
+
+	fprintf(stderr, "typename: ");
+	debug_print_type(type);
+	fprintf(stderr, "\n\n");
+	return;
+}
+
 int main()
 {
-	check("int **argv");
-	check("int (*(*argv))");
-	check("int arr[13]");
-	check("int (*daytab)[13]");
-	check("int (*(*x[3])[2])[4]");
-	check("int (*(*x())[2])()");
-	check("int *comp()");
-	check("int (*comp)()");
-	check("int (*(*x())[2])()");
-	check("int atexit(int (*func)(int a))");
-	check("int (*signal(int sig, int (*func)(int a)))(int b)");
-	check("char **argv");
-	check("char (*(*argv))");
-	check("char arr[13]");
-	check("char (*daytab)[13]");
-	check("char (*(*x[3])[2])[4]");
+	check_declaration("int **argv");
+	check_declaration("int (*(*argv))");
+	check_declaration("int arr[13]");
+	check_declaration("int (*daytab)[13]");
+	check_declaration("int (*(*x[3])[2])[4]");
+	check_declaration("int (*(*x())[2])()");
+	check_declaration("int *comp()");
+	check_declaration("int (*comp)()");
+	check_declaration("int (*(*x())[2])()");
+	check_declaration("int atexit(int (*func)(int a))");
+	check_declaration("int (*signal(int sig, int (*func)(int a)))(int b)");
+	check_declaration("char **argv");
+	check_declaration("char (*(*argv))");
+	check_declaration("char arr[13]");
+	check_declaration("char (*daytab)[13]");
+	check_declaration("char (*(*x[3])[2])[4]");
+
+	check_typename("int **");
+	check_typename("int [13]");
+	check_typename("int (*)[13]");
+	check_typename("int (*(*[3])[2])[4]");
+	check_typename("int (*(*())[2])()");
+	check_typename("int *()");
+	check_typename("int (*)()");
+	check_typename("int (*(*())[2])()");
+	check_typename("int (int (*func)(int a))");
+	check_typename("int (*(int sig, int (*func)(int a)))(int b)");
 	return 0;
 }
