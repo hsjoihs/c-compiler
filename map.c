ddccd7b5bc12bbcb56def0498c1e27c70b89b227
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-27 10:12:12 +0900

    remove Map and use Map2*

--- map.c
+++ map.c
@@ -14,56 +14,45 @@ struct _mapchip {
 };
 
 /* it overrides; it does not overwrite. */
-void insert(struct Map *ptr, const char *key, void *value)
+void insert(struct Map2 **ptr, const char *key, void *value)
 {
 	struct _mapchip a;
 	a.ptr = key;
 	a.value = value;
 
-	if (ptr->internal->_alloc < ptr->internal->_length + 1) {
+	if ((*ptr)->_alloc < (*ptr)->_length + 1) {
 
-		ptr->internal->_vec =
-		    realloc(ptr->internal->_vec,
-		            ptr->internal->_alloc * 2 * sizeof(struct _mapchip));
+		(*ptr)->_vec =
+		    realloc((*ptr)->_vec, (*ptr)->_alloc * 2 * sizeof(struct _mapchip));
 
-		ptr->internal->_alloc *= 2;
+		(*ptr)->_alloc *= 2;
 	}
 
-	ptr->internal->_vec[ptr->internal->_length] = a;
-	++(ptr->internal->_length);
+	(*ptr)->_vec[(*ptr)->_length] = a;
+	++((*ptr)->_length);
 }
 
-void *lookup(const struct Map m, const char *key)
+void *lookup(const struct Map2 *m, const char *key)
 {
-	for (int i = (m.internal->_length) - 1; i >= 0; --i) {
-		if (strcmp(m.internal->_vec[i].ptr, key) == 0) {
-			return m.internal->_vec[i].value;
+	for (int i = (m->_length) - 1; i >= 0; --i) {
+		if (strcmp(m->_vec[i].ptr, key) == 0) {
+			return m->_vec[i].value;
 		}
 	}
 	return 0;
 }
 
-int isElem(const struct Map m, const char *key) { return lookup(m, key) != 0; }
-
-struct Map init_map(void)
+int isElem(const struct Map2 *m, const char *key)
 {
-	struct Map res;
-	res.internal = calloc(1, sizeof(struct Map2));
-	res.internal->_length = 0;
-	res.internal->_alloc = 256;
-	res.internal->_vec = calloc(res.internal->_alloc, sizeof(struct _mapchip));
-	return res;
+	return lookup(m, key) != 0;
 }
 
-struct Map clone_map(const struct Map map)
+struct Map2 *init_map(void)
 {
-	struct Map res;
-	res.internal = calloc(1, sizeof(struct Map2));
-	res.internal->_length = map.internal->_length;
-	res.internal->_alloc = map.internal->_alloc;
-	res.internal->_vec = calloc(res.internal->_alloc, sizeof(struct _mapchip));
-	for (int i = 0; i < res.internal->_length; i++) {
-		res.internal->_vec[i] = map.internal->_vec[i];
-	}
+	struct Map2 *res;
+	res = calloc(1, sizeof(struct Map2));
+	res->_length = 0;
+	res->_alloc = 256;
+	res->_vec = calloc(res->_alloc, sizeof(struct _mapchip));
 	return res;
 }
--- map.h
+++ map.h
@@ -1,11 +1,8 @@
 struct Map2;
 
-struct Map {
-	struct Map2 *internal;
-};
+#define Map Map2 *
 
 void insert(struct Map *map_ptr, const char *key, void *value);
 void *lookup(const struct Map map, const char *key);
 struct Map init_map(void);
 int isElem(const struct Map map, const char *key);
-struct Map clone_map(const struct Map map);
