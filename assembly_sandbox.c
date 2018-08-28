b45d5aacb3c267c5937ad83ed780122b4a4aa069
hsjoihs <hs.ioling.hs@gmail.com>
2018-08-28 09:39:37 +0900

    experiment

--- assembly_sandbox.c
+++ assembly_sandbox.c
@@ -5,46 +5,28 @@ int main()
 {
 	/*
 
+struct A {int a; char c; char d; int b; char e; };
+
 int main()
 {
-	int a[5];
-	int *p = a;
-	int *q = a+3;
-	int r = p < q;
-	if (r) {
-	    return 174;
-	} else {
-	    return 1;
-	}
+
+	struct A a;
+	struct A b;
+	a = b;
+	return 174;
 }
 
 	*/
 	gen_prologue(60, "main");
-
-	gen_push_address_of_local(-48);
-	gen_write_to_local_8byte(-8);
+	gen_push_address_of_local(-16);
+	gen_push_address_of_local(-32);
+	puts("  movq -32(%rbp), %rax\n"
+	     "  movq %rax, -16(%rbp)\n"
+	     "  movq -24(%rbp), %rax\n"
+	     "  movq %rax, -8(%rbp)\n");
 	gen_discard();
-
-	gen_push_address_of_local(-48);
-	gen_push_int(12);
-	gen_cltq();
-	gen_op_8byte("addq");
-	gen_write_to_local_8byte(-16);
 	gen_discard();
-
-	gen_push_from_local_8byte(-8);
-	gen_push_from_local_8byte(-16);
-	gen_compare_ptrs("setb");
-	gen_write_to_local_8byte(-20);
-	gen_discard();
-
-	gen_if_zero_jmp_nbyte(4, 2, -20);
 	gen_push_int(174);
-
-	gen_jump(4, "");
-	gen_label(2);
-	gen_push_int(1);
-
-	gen_epilogue(4);
+	gen_epilogue(543);
 	return 0;
 }
