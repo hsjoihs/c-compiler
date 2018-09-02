900e7dbd2e77ed9a49e737660b6cf7bb3a629014
hsjoihs <hs.ioling.hs@gmail.com>
2018-09-02 21:42:36 +0900

    reduce duplication

--- codegen.c
+++ codegen.c
@@ -81,22 +81,12 @@ static void print_statement(struct PrinterState *ptr_prs,
 	if (sta.category != DECLARATION_STATEMENT) {
 		for (int j = 0; j < sta.labels.length; j++) {
 			const struct SourceLabel *ptr_label = sta.labels.vector[j];
-			if (ptr_label->category == DEFAULT_LABEL) {
-				if (!ptr_prs->is_inside_switch) {
-					simple_error("`default` was detected, but is not inside "
-					             "`switch`.\n");
-				}
-				for (int k = 0; k < ptr_prs->case_default_vec.length; k++) {
-					const struct SourceLabelAndAssemblyLabel *ptr_ll =
-					    ptr_prs->case_default_vec.vector[k];
-					if (is_label_compatible(&ptr_ll->source_label, ptr_label)) {
-						gen_label(ptr_ll->assembly_label);
-					}
-				}
-			} else if (ptr_label->category == CASE_LABEL) {
+			if (ptr_label->category == DEFAULT_LABEL ||
+			    ptr_label->category == CASE_LABEL) {
 				if (!ptr_prs->is_inside_switch) {
 					simple_error(
-					    "`case` was detected, but is not inside `switch`.\n");
+					    "`default` or `case` was detected, but is not inside "
+					    "`switch`.\n");
 				}
 				for (int k = 0; k < ptr_prs->case_default_vec.length; k++) {
 					const struct SourceLabelAndAssemblyLabel *ptr_ll =
