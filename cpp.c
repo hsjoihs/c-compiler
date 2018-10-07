efe31a654c1d10deda3021002439cd7beb29f0f0
hsjoihs <hs.ioling.hs@gmail.com>
2018-10-07 09:48:00 +0900

    invert the condition

--- cpp.c
+++ cpp.c
@@ -93,8 +93,8 @@ static void skip_till_corresponding_endif(const struct Token **ptr_src)
 	}
 }
 
-static int foo(struct Token *dst, const struct Token **ptr_src,
-               int *ptr_j, enum PreprocessorState *ptr_s, struct Map2 *def_map);
+static int foo(struct Token *dst, const struct Token **ptr_src, int *ptr_j,
+               enum PreprocessorState *ptr_s, struct Map2 *def_map);
 
 /* lexer inserts a NEWLINE before END; hence, END can mostly be ignored */
 struct Tokvec preprocess(const char *str, struct Map2 *def_map)
@@ -110,159 +110,160 @@ struct Tokvec preprocess(const char *str, struct Map2 *def_map)
 	int j = 0;
 	enum PreprocessorState s = LINE_HAS_JUST_STARTED;
 	while (1) {
-		if (s == LINE_HAS_JUST_STARTED && src[0].kind == HASH) {
+		if (s != LINE_HAS_JUST_STARTED || src[0].kind != HASH) {
+			if (foo(dst, &src, &j, &s, def_map)) {
+				break;
+			}
+			continue;
+		}
+		src++;
+
+		consume_spaces(&src);
+
+		if (src[0].kind == NEWLINE) { /* empty directive */
+			dst[j] = *src;
+			s = LINE_HAS_JUST_STARTED;
+			j++;
 			src++;
+			continue;
+		}
+
+		expect_and_consume(&src, IDENT_OR_RESERVED,
+		                   "identifier after `#` for preprocessor directive");
+
+		if (strcmp(src[-1].ident_str, "define") == 0) {
+
+			consume_spaces(&src);
+			expect_and_consume(&src, IDENT_OR_RESERVED,
+			                   "macro name after `#define`");
+			const char *macro_name = src[-1].ident_str;
+
+			/* only when the identifier is IMMEDIATELY followed by
+			 * LEFT_PAREN */
+			if (src[0].kind == LEFT_PAREN) {
+				unsupported("function-style macro");
+			}
+
+			/* that's why this must be here, not earlier */
+			consume_spaces(&src);
+
+			struct Token *ptr_token = calloc(1, sizeof(struct Token));
+
+			/* empty replacement */
+			if (src[0].kind == NEWLINE) {
+				src++;
+				s = LINE_HAS_JUST_STARTED;
+				ptr_token->kind = SPACE;
+				insert(def_map, macro_name, ptr_token);
+				continue;
+			}
+
+			if (src[1].kind != NEWLINE) {
+				unsupported(
+				    "`#define` directive that expands to multiple tokens");
+			}
+
+			/* one-token replacement */
+			*ptr_token = *src;
+			insert(def_map, macro_name, ptr_token);
+			src++;
+
+			if (src[0].kind == NEWLINE) {
+				src++;
+				s = LINE_HAS_JUST_STARTED;
+				continue;
+			}
+		} else if (strcmp(src[-1].ident_str, "include") == 0) {
+			consume_spaces(&src);
+
+			if (src[0].kind == OP_LT) {
+				unsupported("`#include <...>`");
+			}
+
+			expect_and_consume(&src, LIT_STRING, "path after `#include`");
+
+			struct __FILE *fp = fopen(src[-1].literal_str, "r");
+
+			if (!fp) {
+				fprintf(stderr, "failed to open file `%s` to be `#include`d.\n",
+				        src[-1].literal_str);
+				exit(EXIT_FAILURE);
+			}
+
+			const char *imported = read_from_file(fp);
+			const struct Tokvec new_vec = preprocess(imported, def_map);
+			/* def_map will be modified, and that's how all the macros will
+			 * be obtained here */
+
+			total_token_num += new_vec.tok_num - 2; /* BEGINNING and END gone */
+
+			dst = realloc(dst,
+			              total_token_num *
+			                  sizeof(struct Token)); /* realloc never fails */
+
+			int l = 1; /* skip BEGINNING */
+			while (1) {
+				if (new_vec.v[l].kind == END) {
+					break;
+				}
+				dst[j] = new_vec.v[l];
+				j++;
+				l++;
+			}
 
 			consume_spaces(&src);
 
-			if (src[0].kind == NEWLINE) { /* empty directive */
-				dst[j] = *src;
-				s = LINE_HAS_JUST_STARTED;
-				j++;
+			if (src[0].kind == NEWLINE) {
 				src++;
+				s = LINE_HAS_JUST_STARTED;
 				continue;
 			}
 
-			expect_and_consume(
-			    &src, IDENT_OR_RESERVED,
-			    "identifier after `#` for preprocessor directive");
+			error_unexpected_token(
+			    src, "newline or end of file after `#include (filepath)`");
 
-			if (strcmp(src[-1].ident_str, "define") == 0) {
+		} else if (strcmp(src[-1].ident_str, "ifdef") == 0 ||
+		           strcmp(src[-1].ident_str, "ifndef") == 0) {
+			int is_ifdef = strcmp(src[-1].ident_str, "ifdef") == 0;
 
-				consume_spaces(&src);
-				expect_and_consume(&src, IDENT_OR_RESERVED,
-				                   "macro name after `#define`");
-				const char *macro_name = src[-1].ident_str;
+			consume_spaces(&src);
+			expect_and_consume(&src, IDENT_OR_RESERVED,
+			                   is_ifdef ? "identifier after `#ifdef`"
+			                            : "identifier after `#ifndef`");
 
-				/* only when the identifier is IMMEDIATELY followed by
-				 * LEFT_PAREN */
-				if (src[0].kind == LEFT_PAREN) {
-					unsupported("function-style macro");
-				}
+			const char *macro_name = src[-1].ident_str;
+			consume_spaces(&src);
 
-				/* that's why this must be here, not earlier */
-				consume_spaces(&src);
+			expect_and_consume(&src, NEWLINE,
+			                   "newline after `#if%sdef (macro_name)`");
+			src--; /* ad hoc */
 
-				struct Token *ptr_token = calloc(1, sizeof(struct Token));
+			s = LINE_HAS_JUST_STARTED;
 
-				/* empty replacement */
-				if (src[0].kind == NEWLINE) {
-					src++;
-					s = LINE_HAS_JUST_STARTED;
-					ptr_token->kind = SPACE;
-					insert(def_map, macro_name, ptr_token);
-					continue;
-				}
-
-				if (src[1].kind != NEWLINE) {
-					unsupported(
-					    "`#define` directive that expands to multiple tokens");
-				}
-
-				/* one-token replacement */
-				*ptr_token = *src;
-				insert(def_map, macro_name, ptr_token);
-				src++;
-
-				if (src[0].kind == NEWLINE) {
-					src++;
-					s = LINE_HAS_JUST_STARTED;
-					continue;
-				}
-			} else if (strcmp(src[-1].ident_str, "include") == 0) {
-				consume_spaces(&src);
-
-				if (src[0].kind == OP_LT) {
-					unsupported("`#include <...>`");
-				}
-
-				expect_and_consume(&src, LIT_STRING, "path after `#include`");
-
-				struct __FILE *fp = fopen(src[-1].literal_str, "r");
-
-				if (!fp) {
-					fprintf(stderr,
-					        "failed to open file `%s` to be `#include`d.\n",
-					        src[-1].literal_str);
-					exit(EXIT_FAILURE);
-				}
-
-				const char *imported = read_from_file(fp);
-				const struct Tokvec new_vec = preprocess(imported, def_map);
-				/* def_map will be modified, and that's how all the macros will
-				 * be obtained here */
-
-				total_token_num +=
-				    new_vec.tok_num - 2; /* BEGINNING and END gone */
-
-				dst = realloc(
-				    dst, total_token_num *
-				             sizeof(struct Token)); /* realloc never fails */
-
-				int l = 1; /* skip BEGINNING */
-				while (1) {
-					if (new_vec.v[l].kind == END) {
-						break;
-					}
-					dst[j] = new_vec.v[l];
-					j++;
-					l++;
-				}
-
-				consume_spaces(&src);
-
-				if (src[0].kind == NEWLINE) {
-					src++;
-					s = LINE_HAS_JUST_STARTED;
-					continue;
-				}
-
-				error_unexpected_token(
-				    src, "newline or end of file after `#include (filepath)`");
-
-			} else if (strcmp(src[-1].ident_str, "ifdef") == 0 ||
-			           strcmp(src[-1].ident_str, "ifndef") == 0) {
-				int is_ifdef = strcmp(src[-1].ident_str, "ifdef") == 0;
-
-				consume_spaces(&src);
-				expect_and_consume(&src, IDENT_OR_RESERVED,
-				                   is_ifdef ? "identifier after `#ifdef`"
-				                            : "identifier after `#ifndef`");
-
-				const char *macro_name = src[-1].ident_str;
-				consume_spaces(&src);
-
-				expect_and_consume(&src, NEWLINE,
-				                   "newline after `#if%sdef (macro_name)`");
-				src--; /* ad hoc */
-
-				s = LINE_HAS_JUST_STARTED;
-
-				if (is_ifdef == isElem(def_map, macro_name)) { /* true branch */
-					ifdef_depth++;
-				} else { /* false branch */
-					skip_till_corresponding_endif(&src);
-					continue;
-				}
-
-			} else if (strcmp(src[-1].ident_str, "endif") ==
-			           0) { /* passes only when the #if(n)?def condition was
-				               true. If false, it will be handled in
-				               #if(n)?def.*/
-				if (ifdef_depth < 1) {
-					fprintf(stderr,
-					        "mismatch of `#endif` directive was detected.\n");
-					exit(EXIT_FAILURE);
-				}
-
-				ifdef_depth--;
-				consume_spaces(&src);
-				expect_and_consume(&src, NEWLINE, "newline after `#endif`");
-				s = LINE_HAS_JUST_STARTED;
+			if (is_ifdef == isElem(def_map, macro_name)) { /* true branch */
+				ifdef_depth++;
+			} else { /* false branch */
+				skip_till_corresponding_endif(&src);
 				continue;
-			} else {
-				unsupported("unknown directive");
 			}
+
+		} else if (strcmp(src[-1].ident_str, "endif") ==
+		           0) { /* passes only when the #if(n)?def condition was
+			               true. If false, it will be handled in
+			               #if(n)?def.*/
+			if (ifdef_depth < 1) {
+				fprintf(stderr,
+				        "mismatch of `#endif` directive was detected.\n");
+				exit(EXIT_FAILURE);
+			}
+
+			ifdef_depth--;
+			consume_spaces(&src);
+			expect_and_consume(&src, NEWLINE, "newline after `#endif`");
+			s = LINE_HAS_JUST_STARTED;
+			continue;
+		} else {
+			unsupported("unknown directive");
 		}
 
 		if (foo(dst, &src, &j, &s, def_map)) {
@@ -282,8 +283,8 @@ struct Tokvec preprocess(const char *str, struct Map2 *def_map)
 	return u;
 }
 
-static int foo(struct Token *dst, const struct Token **ptr_src,
-               int *ptr_j, enum PreprocessorState *ptr_s, struct Map2 *def_map)
+static int foo(struct Token *dst, const struct Token **ptr_src, int *ptr_j,
+               enum PreprocessorState *ptr_s, struct Map2 *def_map)
 {
 	const struct Token *src = *ptr_src;
 	int j = *ptr_j;
