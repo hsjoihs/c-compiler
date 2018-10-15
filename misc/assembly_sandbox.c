00abe57897a3d2566917c527beac7f0055fb013e
hsjoihs <hs.ioling.hs@gmail.com>
2018-10-16 04:01:18 +0900

    at least works in Mac

--- misc/assembly_sandbox.c
+++ misc/assembly_sandbox.c
@@ -4,117 +4,68 @@
 int main()
 {
 	/*
-struct B {int a; int b;};
-struct D {int a; int b; char c;};
-struct F {int k; int *a; int *b; char *c; char *d; int *e;};
-
-struct B foo()
-{
-	struct B s;
-	s.a = 1;
-	return s;
+int a(void* q){
+	int (*p)(int) = q;
+	return (*p)(171);
 }
 
-struct D bar()
-{
-	struct D s;
-	s.a = 1;
-	return s;
+int f(int a){
+	return 3 + a;
 }
 
-
-struct F baz()
+void *b(void)
 {
-	struct F s;
-	s.k = 172;
-	return s;
+	return f;
 }
-
 
 int main()
 {
-	int num = 0;
-	struct B b;
-	b = foo();
-	num += b.a;
-
-	struct D a;
-	a = bar();
-	num += a.a;
-
-	struct F c;
-	c = baz();
-	num += c.k;
-
-	return num;
+	return a(b());
 }
-
 	*/
-	gen_prologue(8, "foo");
-	puts("  movl $1, -8(%rbp)\n");
-	gen_push_address_of_local(-8);
-	gen_epilogue_returning_small_struct(8, 5623);
-
-	{
-		gen_prologue(20, "bar");
-
-		gen_push_int(1);
-		gen_write_to_local(-24);
-		gen_discard();
-		puts(
-
-		    "  movq -24(%rbp), %rax\n"
-		    "  movq %rax, -12(%rbp)\n"
-
-		    "  movl -16(%rbp), %eax\n"
-		    "  movl %eax, -4(%rbp)\n");
-
-		gen_push_address_of_local(-12);
-		gen_epilogue_returning_small_struct(12, 875);
-	}
-
-	{
-		/* baz */
-		gen_prologue(8, "baz");
-
-		gen_write_register_to_local_8byte("rdi", -56);
-
-		gen_push_int(172);
-		gen_write_to_local(-48);
-		gen_discard();
-
-		gen_push_from_local_8byte(-56);
-		gen_push_address_of_local(-48);
-		gen_copy_struct_and_discard(48);
-
-		gen_epilogue_8byte(5463);
-	}
-
-	gen_prologue(160, "main");
-	gen_push_int(0);
-	gen_write_to_local(-4);
-	gen_discard();
-
-	gen_call_and_assign_small_struct_to_local("foo", -12, 8);
-	puts("  movl -12(%rbp), %eax\n"
-	     "  addl %eax, -4(%rbp)\n"
-	     "  addq $8, %rsp\n");
-
-	gen_call_and_assign_small_struct_to_local("bar", -24, 12);
-	
 	puts(
-	     "  movl -24(%rbp), %eax\n"
-	     "  addl %eax, -4(%rbp)\n"
-	     "  addq $8, %rsp\n"
-	     "  leaq -80(%rbp), %rax\n"
-	     "  movq %rax, %rdi\n"
-	     "  movl $0, %eax\n"
-	     "  call " PREFIX "baz\n"
-	     "  movl -80(%rbp), %eax\n"
-	     "  addl %eax, -4(%rbp)\n"
-	     "  subq $8, %rsp\n"
-	     "  movl -4(%rbp), %eax\n"
-	     "  movl  %eax, (%rsp)\n");
-	gen_epilogue(425);
+	    "	.globl	"PREFIX"a                      ## -- Begin function a\n"
+	    ""PREFIX"a:                                     ## @a\n"
+	    "	pushq	%rbp\n"
+	    "	movq	%rsp, %rbp\n"
+	    "	subq	$16, %rsp\n"
+	    "	movl	$171, %eax\n"
+	    "	movq	%rdi, -8(%rbp)\n"
+	    "	movq	-8(%rbp), %rdi\n"
+	    "	movq	%rdi, -16(%rbp)\n"
+	    "	movl	%eax, %edi\n"
+	    "	callq	*-16(%rbp)\n"
+	    "	addq	$16, %rsp\n"
+	    "	popq	%rbp\n"
+	    "	retq\n");
+	puts("	.globl	"PREFIX"f                      ## -- Begin function f\n"
+	     ""PREFIX"f:                                     ## @f\n"
+	     "	pushq	%rbp\n"
+	     "	movq	%rsp, %rbp\n"
+	     "	movl	%edi, -4(%rbp)\n"
+	     "	movl	-4(%rbp), %edi\n"
+	     "	addl	$3, %edi\n"
+	     "	movl	%edi, %eax\n"
+	     "	popq	%rbp\n"
+	     "	retq\n");
+	puts("	.globl	"PREFIX"b                      ## -- Begin function b\n"
+	     ""PREFIX"b:                                     ## @b\n"
+	     "	pushq	%rbp\n"
+	     "	movq	%rsp, %rbp\n"
+	     "	leaq	"PREFIX"f(%rip), %rax\n"
+	     "	popq	%rbp\n"
+	     "	retq\n");
+	puts("	.globl	"PREFIX"main                   ## -- Begin function main\n"
+	     ""PREFIX"main:                                  ## @main\n"
+	     "	pushq	%rbp\n"
+	     "	movq	%rsp, %rbp\n"
+	     "	subq	$16, %rsp\n"
+	     "	movl	$0, -4(%rbp)\n"
+	     "	callq	"PREFIX"b\n"
+	     "	movq	%rax, %rdi\n"
+	     "	callq	"PREFIX"a\n"
+	     "	addq	$16, %rsp\n"
+	     "	popq	%rbp\n"
+	     "	retq\n");
 	return 0;
 }
