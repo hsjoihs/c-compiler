30c8ad80435aa241adeb8dc7117c757bcd6ae5d0
hsjoihs <hs.ioling.hs@gmail.com>
2018-10-25 01:35:19 +0900

    only disclose 53 functions

--- misc/assembly_sandbox.c
+++ misc/assembly_sandbox.c
@@ -11,18 +11,18 @@ int a(void* q){
 	*/
 	gen_prologue(100, "a");
 	gen_write_register_to_local_8byte("rdi", -8); // void *q
-	gen_push_from_local_8byte(-8);
+	gen_push_from_local_nbyte(8, -8);
 	gen_write_to_local_8byte(-16); // p = q;
 	gen_discard();
 	
 	gen_push_int(171);
 	gen_pop_to_reg_4byte("edi");
 
-	gen_push_from_local_8byte(-16);
+	gen_push_from_local_nbyte(8, -16);
 	gen_pop_to_reg_8byte("r11");
 
-	gen_call_reg_and_push_ret_of_4byte("r11");
-	gen_epilogue(1);
+	gen_call_reg_and_push_ret_of_nbyte(4, "r11");
+	gen_epilogue_nbyte(4, 1);
 
 	/*
 int f(int a){
@@ -31,10 +31,10 @@ int f(int a){
 	*/
 	gen_prologue(0, "f");
 	gen_write_register_to_local_4byte("edi", -4);
-	gen_push_from_local_4byte(-4);
+	gen_push_from_local_nbyte(4, -4);
 	gen_push_int(3);
 	gen_op_ints("addl");
-	gen_epilogue(2);
+	gen_epilogue_nbyte(4, 2);
 
 	/*
 void *b(void)
@@ -44,7 +44,7 @@ void *b(void)
 	*/
 	gen_prologue(0, "b");
 	gen_push_address_of_global("f");
-	gen_epilogue_8byte(3);
+	gen_epilogue_nbyte(8, 3);
 	
 	/*
 int main()
@@ -53,10 +53,10 @@ int main()
 }
 	*/
 	gen_prologue(16, "main");
-	gen_push_ret_of_8byte("b");
+	gen_push_ret_of_nbyte(8, "b");
 	gen_pop_to_reg_8byte("rdi");
-	gen_push_ret_of_8byte("a");
-	gen_epilogue_8byte(4);
+	gen_push_ret_of_nbyte(8, "a");
+	gen_epilogue_nbyte(8, 4);
 	
 	return 0;
 }
