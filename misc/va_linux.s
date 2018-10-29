0051be793fc92c819b82476bb73479fe3aae6abc
hsjoihs <hs.ioling.hs@gmail.com>
2018-10-30 04:33:47 +0900

    reduce va_linux.c

--- misc/va_linux.s
+++ misc/va_linux.s
@@ -1,20 +1,9 @@
-	.file	"va.c"
-	.text
-	.p2align 4,,15
 	.globl	debug_write
-	.type	debug_write, @function
 debug_write:
-.LFB23:
-	.cfi_startproc
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	pushq	%rbx
-	.cfi_def_cfa_offset 24
-	.cfi_offset 3, -24
 	movq	%rdi, %rbx
 	subq	$216, %rsp
-	.cfi_def_cfa_offset 240
 	testb	%al, %al
 	movq	%rsi, 40(%rsp)
 	movq	%rdx, 48(%rsp)
@@ -61,18 +50,8 @@ debug_write:
 	xorq	%fs:40, %rax
 	jne	.L6
 	addq	$216, %rsp
-	.cfi_remember_state
-	.cfi_def_cfa_offset 24
 	popq	%rbx
-	.cfi_def_cfa_offset 16
 	popq	%rbp
-	.cfi_def_cfa_offset 8
 	ret
 .L6:
-	.cfi_restore_state
 	call	__stack_chk_fail@PLT
-	.cfi_endproc
-.LFE23:
-	.size	debug_write, .-debug_write
-	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
-	.section	.note.GNU-stack,"",@progbits
