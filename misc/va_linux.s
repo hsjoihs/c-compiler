1f1aa1897bd7265b3fb2f6967a870da999ba6fd2
hsjoihs <hs.ioling.hs@gmail.com>
2018-10-30 21:08:24 +0900

    Edit linux's assembly so that it matches mac's better

--- misc/va_linux.s
+++ misc/va_linux.s
@@ -1,24 +1,25 @@
 	.globl	debug_write
 debug_write:
 	pushq	%rbp
+	movq	%rsp, %rbp
 	pushq	%rbx
 	movq	%rdi, %rbx
 	subq	$216, %rsp
 	testb	%al, %al
-	movq	%rsi, 40(%rsp)
-	movq	%rdx, 48(%rsp)
-	movq	%rcx, 56(%rsp)
-	movq	%r8, 64(%rsp)
-	movq	%r9, 72(%rsp)
+	movq	%rsi, -184(%rbp)
+	movq	%rdx, -176(%rbp)
+	movq	%rcx, -168(%rbp)
+	movq	%r8, -160(%rbp)
+	movq	%r9, -152(%rbp)
 	je	.L2
-	movaps	%xmm0, 80(%rsp)
-	movaps	%xmm1, 96(%rsp)
-	movaps	%xmm2, 112(%rsp)
-	movaps	%xmm3, 128(%rsp)
-	movaps	%xmm4, 144(%rsp)
-	movaps	%xmm5, 160(%rsp)
-	movaps	%xmm6, 176(%rsp)
-	movaps	%xmm7, 192(%rsp)
+	movaps	%xmm0, -144(%rbp)
+	movaps	%xmm1, -128(%rbp)
+	movaps	%xmm2, -112(%rbp)
+	movaps	%xmm3, -96(%rbp)
+	movaps	%xmm4, -80(%rbp)
+	movaps	%xmm5, -64(%rbp)
+	movaps	%xmm6, -48(%rbp)
+	movaps	%xmm7, -32(%rbp)
 .L2:
 	movq	%fs:40, %rax
 	movq	%rax, 24(%rsp)
@@ -55,3 +56,4 @@ debug_write:
 	ret
 .L6:
 	call	__stack_chk_fail@PLT
+
