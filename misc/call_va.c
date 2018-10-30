5411a0e9f8063e765551b73d36a4fb72cb2cee89
hsjoihs <hs.ioling.hs@gmail.com>
2018-10-31 01:31:34 +0900

    more test cases

--- misc/call_va.c
+++ misc/call_va.c
@@ -6,4 +6,6 @@ int main()
     debug_write("%s", "Hello, va_list 2!\n");
     debug_write("%s %s %d%c\n", "Hello,", "va_list", 3, '!');
     debug_write("%s %s %f%c\n", "Hello,", "va_list", 4.0, '!');
+    debug_write("%f %f %f %f %f %f %f %f%c\n", 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, '!');
+    debug_write("%s%s%c %s%s%s%s %f%c\n", "He", "llo", ',', "va", "_", "li", "st", 13.0, '!');
 }
